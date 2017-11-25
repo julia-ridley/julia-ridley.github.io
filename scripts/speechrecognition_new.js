e3efb401c6631ca34ad66b7fb60c19f1f7f17a91
julia-ridley <32227285+julia-ridley@users.noreply.github.com>
2017-11-24 22:21:18 -0500

    Add files via upload

---
+++ scripts/speechrecognition_new.js
@@ -0,0 +1,97 @@
+var lastTextField = null;
+
+$(document).ready(function() {
+	//console.log("hello there!");
+	$("input[type='text'],textarea").click(function() {
+		if (document.getElementById("command")!=this) {
+			lastTextField = this;
+			console.log(lastTextField);
+		}
+		
+	});
+
+	recognition.onresult = function(event) {
+		var potentialcommand = $("#recognized_speech").val();
+		console.log("input: "+potentialcommand);
+
+		//var re = /^scroll\s.*(down|up).*/i //starts with scroll, then any number of upper/lowercase characters
+		var re = /^(click|scroll|enter)\s(.*)/i 
+		var result = re.exec(potentialcommand)
+		console.log(result);
+
+
+		if (result) { 
+			var verb = result[1];
+			var arg = result[2];
+
+			//$("*:contains"('"+arg="'))
+
+			console.log("verb: "+verb+", args: "+arg);
+			//console.log("command: scroll"+result[2])
+			switch(verb) {
+				case "click":
+					console.log("handling click");
+					var stringpieces = arg.split(/\s/);
+					$("a, input, button").each(function() {
+						if ($(this).val()==arg||$(this).text()==arg) { //look for the text in the input field
+							console.log("found the click thing!");
+							simulateClick(this);
+						} //else if () {//other kinds of tags
+
+						//}
+
+					})
+					break;
+				case "scroll":
+					console.log("handling scroll");
+					if (arg=="left") {
+						$("html, body").animate({
+							scrollTop: $(document).scrollLeft()-300
+						},1000);
+					} else if (arg=="right") {
+						$("html, body").animate({
+							scrollTop: $(document).scrollLeft()+300
+						},1000);
+					} else if (arg=="up") {
+						$("html, body").animate({
+							scrollTop: $(document).scrollTop()-300
+						},1000);
+					} else if (arg=="down") {
+						$("html, body").animate({
+							scrollTop: $(document).scrollTop()+300
+						},1000);
+					}
+					break;
+				case "enter":
+					console.log("handling enter");
+					lastTextField.focus();
+					var text = $(lastTextField).val();
+					text += arg
+					$(lastTextField).val(text);
+					break;
+				default:
+					alert("sorry, that's not a recognized command");
+					//any other case
+					break;
+
+			}
+		} else if (!result){
+			alert("I cannot process that command");
+		}
+
+	}
+});
+
+function simulateClick(element) {
+	  	if (!element) return;
+	  		var dispatchEvent = function (elt, name) {
+	   	 	var clickEvent = document.createEvent('MouseEvents');
+	    	clickEvent.initEvent(name, true, true);
+	    	elt.dispatchEvent(clickEvent);
+	  	};
+	  	dispatchEvent(element, 'mouseover');
+	  	dispatchEvent(element, 'mousedown');
+	  	dispatchEvent(element, 'click');
+	  	dispatchEvent(element, 'mouseup');
+	  	console.log("element clicked!")
+	};
