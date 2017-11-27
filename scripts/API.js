a76e47a82c270b78c426444c1b960d4587f8b8f4
julia-ridley <32227285+julia-ridley@users.noreply.github.com>
2017-11-26 19:35:59 -0500

    Update API.js

--- scripts/API.js
+++ scripts/API.js
@@ -150,10 +150,12 @@ if (!('webkitSpeechRecognition' in window)) {
         //console.log("command: scroll"+result[2])
         switch(verb) {
           case "Click":
-            console.log("handling click");
+            //console.log("handling click");
             arg = arg.split(/\s/);
             $("a, input, button").each(function() {
-              console.log(this);
+              //console.log(this);
+              console.log($(this).text());
+              console.log($(this).val());
               if ($(this).val().localeCompare(arg)==0||$(this).text().localeCompare(arg)==0) {
               //if ($(this).val()==arg||$(this).text()==arg) { //look for the text in the input or text field
                 console.log("found the click thing!");
